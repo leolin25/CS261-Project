58639fb617f359c75b493480868ea98d86f2c178
leolin25 <leolinuk25@gmail.com>
2026-01-16 20:28:59 +0000

    Added all stats

--- src/logic/Controller.java
+++ src/logic/Controller.java
@@ -20,7 +20,103 @@ public class Controller {
         // Every second, this will reduce fuel and check for arrivals
     }
 
-    public double getAverageWaitTime() {
+    // Takeoff time data (minutes)
+    public double getTakeoffVariance() {
+        return 0.0;
+    }
+
+    public double getAverageTakeoff() {
+        return 0.0;
+    }
+
+    public double getRangeTakeoff() {
+        return 0.0;
+    }
+
+    public double getUpperQuartileTakeoff() {
+        return 0.0;
+    }
+
+    public double getLowerQuartileTakeoff() {
+        return 0.0;
+    }
+
+    public double getMaxTakeoff() {
+        return 0.0;
+    }
+
+    // Arrival time data (minutes)
+    public double getArrivalVariance() {
+        return 0.0;
+    }
+
+    public double getAverageArrival() {
+        return 0.0;
+    }
+
+    public double getRangeArrival() {
+        return 0.0;
+    }
+
+    public double getUpperQuartileArrival() {
+        return 0.0;
+    }
+
+    public double getLowerQuartileArrival() {
+        return 0.0;
+    }
+
+    public double getMaxArrival() {
+        return 0.0;
+    }
+
+    // Holding queue time data (minutes)
+    public double getAverageHoldingQueueTime() {
+        return 0.0;
+    }
+
+    public double getVarianceHoldingQueueTime() {
+        return 0.0;
+    }
+
+    public double getRangeHoldingQueueTime() {
+        return 0.0;
+    }
+
+    public double getLowerQuartileHoldingQueueTime() {
+        return 0.0;
+    }
+
+    public double getUpperQuartileHoldingQueueTime() {
+        return 0.0;
+    }
+
+    public double getMaxHoldingQueueTime() {
+        return 0.0;
+    }
+
+    // Takeoff queue time data (minutes)
+    public double getAverageTakeoffQueueTime() {
+        return 0.0;
+    }
+
+    public double getVarianceTakeoffQueueTime() {
+        return 0.0;
+    }
+
+    public double getRangeTakeoffQueueTime() {
+        return 0.0;
+    }
+
+    public double getLowerQuartileTakeoffQueueTime() {
+        return 0.0;
+    }
+
+    public double getUpperQuartileTakeoffQueueTime() {
+        return 0.0;
+    }
+
+    public double getMaxTakeoffQueueTime() {
         return 0.0;
     }
 }

148227441e24454e46f756d6e1f922c675d09ab3
Mridul Dhall <mriduldhall1@gmail.com>
2026-02-19 19:35:36 +0000

    Generate normally distributed delay

--- src/simulation/generation.py
+++ src/simulation/generation.py
@@ -1,4 +1,5 @@
 from models import Aircraft
+from numpy import random
 
 class Generator:
     def __init__(self, hour_limit, inbound_per_hour, outbound_per_hour):
@@ -8,6 +9,12 @@ class Generator:
         self.inbound = inbound_per_hour
         self.outbound = outbound_per_hour
         self.last_generated = None
+
+    @staticmethod
+    def generate_sample():
+        # Average delay of 0 minutes with a standard deviation of 5 minutes
+        return int(round(random.normal(loc=0, scale=5)))
+
 
 if __name__ == '__main__':
     pass

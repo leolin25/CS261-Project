03ab0cd0e22e6a3cd03a00c94faeea5ad964ac4f
Mridul Dhall <mriduldhall1@gmail.com>
2026-02-19 21:28:42 +0000

    Generate aircraft with fuel and time data

--- src/simulation/generation.py
+++ src/simulation/generation.py
@@ -1,5 +1,6 @@
-from models import Aircraft
+from .models import Aircraft
 from numpy import random
+from datetime import timedelta, datetime
 
 class Generator:
     def __init__(self, hour_limit, inbound_per_hour, outbound_per_hour):
@@ -8,10 +9,11 @@ class Generator:
         self.hour_limit = hour_limit
         self.inbound = inbound_per_hour
         self.outbound = outbound_per_hour
-        self.last_generated = None
+        self.last_generated_inbound = datetime.now()
+        self.last_generated_outbound = datetime.now()
 
     @staticmethod
-    def generate_sample():
+    def generate_delay():
         # Average delay of 0 minutes with a standard deviation of 5 minutes
         return int(round(random.normal(loc=0, scale=5)))
 
@@ -20,5 +22,31 @@ class Generator:
         # Random fuel between 20 and 60 minutes for arrivals, 180 to 300 for departures
         return random.randint(20, 60)
 
-if __name__ == '__main__':
-    pass
+    def generate_aircraft(self, is_arrival):
+        delay = self.generate_delay()
+        if is_arrival:
+            scheduled_time = self.last_generated_inbound + timedelta(minutes=round(60/self.inbound))
+        else:
+            scheduled_time = self.last_generated_outbound + timedelta(minutes=round(60/self.outbound))
+        expected_time = scheduled_time + timedelta(minutes=delay)
+        fuel = self.generate_fuel()
+        aircraft = Aircraft(
+            callsign="123",
+            operator="TEST",
+            origin="AAA",
+            destination="BBB",
+            scheduled_arrival=scheduled_time,
+            scheduled_departure=scheduled_time,
+            queue_entry_time=expected_time,
+            altitude=1000,
+            fuel_mins=fuel,
+            emergency_status='NONE',
+            zone_status='SCHEDULED'
+        )
+        if is_arrival:
+            self.last_generated_inbound = scheduled_time
+            self.inbound_schedule.append(aircraft)
+        else:
+            self.last_generated_outbound = scheduled_time
+            self.outbound_schedule.append(aircraft)
+        return aircraft

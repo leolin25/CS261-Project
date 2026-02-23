59a89449de65ba1f87c277a4aefacc4f859601c1
Ethan <ethanwbeard@gmail.com>
2026-02-23 15:42:59 +0000

    simulation page recieving json data from random generator, updating every second

--- src/simulation/urls.py
+++ src/simulation/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import home, results, simulation, GetSampleData
+from .views import home, results, simulation, stream, GetSampleData
 
 urlpatterns = [
     path('', home, name='home'),
     path('results/', results, name='results'),
     path('simulation/',simulation, name='simulation'),
     path('api/sample-data/', GetSampleData.as_view(), name='sample-data'),
-    path('stream/', views.stream, name='stream')
+    path('stream/',stream, name='stream')
 
 ]
--- src/simulation/views.py
+++ src/simulation/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.test import Client
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -59,6 +60,12 @@ class GetSampleData(APIView):
         inbound, outbound = generator.run_generation()
         serializer = self.serializer_class(inbound + outbound, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def getinternal(self):
+        generator = Generator(2, 15, 15)
+        inbound, outbound = generator.run_generation()
+        serializer = self.serializer_class(inbound + outbound, many=True)
+        return serializer.data
 
 
 #Stream test using date time
@@ -66,11 +73,9 @@ def stream(request):
     def event_stream():
         while True:
             time.sleep(1)
+            provider = GetSampleData()
+            takeoffQ = provider.getinternal()
             #yield 'data: The server time is: %s\n\n' % datetime.datetime.now()
-            takeoffQ = {
-                "examplefield": "exampledata",
-                "examplefield2": "exampledata2"
-            }
             data = f"data: {json.dumps(takeoffQ)}\n\n" #neeed to define takeoffQ, should just be a json file 
             yield data
     

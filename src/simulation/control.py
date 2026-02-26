a4219ac698d685c01bd32a0ec9f4da3030ff1090
Mridul Dhall <mriduldhall1@gmail.com>
2026-02-26 12:32:22 +0000

    Update plane cancellations dynamically

--- src/simulation/control.py
+++ src/simulation/control.py
@@ -5,7 +5,7 @@ from .generation import Generator
 
 
 class Controller:
-    def __init__(self, inbound_per_hour, outbound_per_hour, launch_time=None, timescale=1, schedule_limit=12):
+    def __init__(self, inbound_per_hour, outbound_per_hour, launch_time=None, timescale=1, schedule_limit=12, max_wait=30):
         start_time = launch_time if launch_time else timezone.now()
         self.inbound_per_hour = inbound_per_hour
         self.outbound_per_hour = outbound_per_hour
@@ -13,6 +13,7 @@ class Controller:
         self.simulation_time = start_time
         self.timescale = timescale #1 second to 1 minute by default
         self.schedule_limit = schedule_limit
+        self.max_wait = max_wait
         self.generator = Generator(self.schedule_limit, self.inbound_per_hour, self.outbound_per_hour, start_time)
 
     def calculate_new_time(self, time_elapsed):
@@ -27,6 +28,18 @@ class Controller:
                 aircraft.zone_status = "QUEUE_TO"
         Aircraft.objects.bulk_update(aircrafts, ['zone_status'])
 
+    def update_aircraft_cancellations(self):
+        aircrafts = Aircraft.objects.filter(zone_status='QUEUE_TO')
+        number_cancelled = 0
+        for aircraft in aircrafts:
+            wait_duration = (self.simulation_time - aircraft.queue_entry_time).total_seconds() / 60
+            if wait_duration > self.max_wait:
+                aircraft.zone_status = 'CANCELLED'
+                number_cancelled += 1
+                print("Flight {} cancelled".format(aircraft.callsign))
+        Aircraft.objects.bulk_update(aircrafts, ['zone_status'])
+        return number_cancelled
+
     @staticmethod
     def get_stream_data():
         inbound_schedule = Aircraft.objects.filter(zone_status="SCHEDULED", scheduled_departure=None).order_by('queue_entry_time')
@@ -36,9 +49,10 @@ class Controller:
     def run_simulation(self):
         update_start_time = timezone.now()
         self.calculate_new_time((update_start_time - self.last_update_time).total_seconds())
-        print(f"Simulation Time: {self.simulation_time}, Real Time: {update_start_time}")
+        #print(f"Simulation Time: {self.simulation_time}, Real Time: {update_start_time}")
         self.generator.run_generation(self.simulation_time)
         self.update_aircraft_statuses()
+        self.update_aircraft_cancellations()
         self.last_update_time = update_start_time
 
     def setup_simulation(self):

944bcc2e70627225317b6d1ac5666de0e26b937b
Mridul Dhall <mriduldhall1@gmail.com>
2026-03-04 17:34:57 +0000

    Ensure runways not occupied before optimising them

--- src/simulation/RunwayController.py
+++ src/simulation/RunwayController.py
@@ -4,9 +4,10 @@ from .models import Aircraft, Runway
 
 
 class RunwayController:
-    def __init__(self, landing_duration=45, takeoff_duration=45):
+    def __init__(self, landing_duration=45, takeoff_duration=45, risk_threshold=25):
         self.landing_duration = landing_duration
         self.takeoff_duration = takeoff_duration
+        self.risk_threshold = risk_threshold
 
     # Assigns a given aircraft to a runway based on its zone status (arriving or departing) and the runway's operating mode. Returns True if a runway is assigned and False is not. Also updates the runway's status to OCCUPIED and saves the assigned runway number in the aircraft's record.
     @staticmethod
@@ -75,28 +76,24 @@ class RunwayController:
         except Runway.DoesNotExist:
             return False
         
-    # This function is called every minute to update the mixed runways based on current traffic, mixed runways are put in a temporary new optimised mode that suits the sitation
-    def optimise_runway_mode(self) -> None:
-        now = timezone.now()
-        twenty_five_mins_ago = now - timedelta(minutes=25)
+    # This function is called every minute to update the mixed runways based on current traffic, mixed runways are put in a temporary new optimised mode that suits the situation
+    def optimise_runway_mode(self, simulation_time):
+        risk_time = simulation_time - timedelta(minutes=self.risk_threshold)
 
         # In case of any bugs we can reset runways right here
         self.reset_optimised_runways()
 
         # Find all mixed runways that are currently available
-        mixed_runways = Runway.objects.filter(operational_status='AVAILABLE', operating_mode__icontains='MIXED')
+        mixed_runways = Runway.objects.filter(operational_status='AVAILABLE', operating_mode__icontains='MIXED', occupied_by__isnull=True)
         if not mixed_runways.exists():
             return
         
         # Emergency risks are the most important, calculate them first and that sets the lower bound of landing runways
         emergency_count = Aircraft.objects.filter(zone_status='QUEUE_LA').exclude(emergency_status='NONE').count()
-        
         # Calculate the number of diversion risks for arrivals 
-        arrival_risks = Aircraft.objects.filter(zone_status='QUEUE_LA', queue_entry_time__lte=twenty_five_mins_ago).count()
-        
+        arrival_risks = Aircraft.objects.filter(zone_status='QUEUE_LA', queue_entry_time__lte=risk_time).count()
         # Cancellation risks are only relevant for takeoff
-        takeoff_risks = Aircraft.objects.filter(zone_status='QUEUE_TO', queue_entry_time__lte=twenty_five_mins_ago).count()
-
+        takeoff_risks = Aircraft.objects.filter(zone_status='QUEUE_TO', queue_entry_time__lte=risk_time).count()
         # Calculate the total amount of arriving and departing planes in the queues to use as a tiebreaker if there are no emergency or cancellation risks. We prioritise the one with more traffic to try and reduce the queues overall, as well as prioritising takeoff if there is a tie to try and reduce congestion on the runways, as planes that are about to takeoff have already been waiting on the runway and are more likely to have been delayed by other planes, whereas arriving planes have not yet reached the runway and so are less likely to have been delayed by other planes.
         amount_arriving = Aircraft.objects.filter(zone_status='QUEUE_LA').count()
         amount_departing = Aircraft.objects.filter(zone_status='QUEUE_TO').count()
@@ -108,42 +105,33 @@ class RunwayController:
                 amount_arriving -= 1
                 runway.operating_mode = 'LANDING'
                 runway.temp_optimised = True
-                runway.save()
             # Prioritise landing if there are more emergency risks, otherwise prioritise takeoff if there are more cancellation risks.
-            elif arrival_risks > takeoff_risks:
+            elif arrival_risks >= takeoff_risks:
                 arrival_risks -= 1
                 amount_arriving -= 1
                 runway.operating_mode = 'LANDING'
                 runway.temp_optimised = True
-                runway.save()
             elif takeoff_risks > arrival_risks:
                 takeoff_risks -= 1
                 amount_departing -= 1
                 runway.operating_mode = 'TAKEOFF'
                 runway.temp_optimised = True
-                runway.save()
             # No emergency or cancellation risks, so optimise based on which has more traffic
-            elif amount_arriving > amount_departing:
+            elif amount_arriving >= amount_departing:
                 amount_arriving -= 1
                 runway.operating_mode = 'LANDING'
                 runway.temp_optimised = True
-                runway.save()
             elif amount_departing > amount_arriving:
                 amount_departing -= 1
                 runway.operating_mode = 'TAKEOFF'
                 runway.temp_optimised = True
-                runway.save()
-            # Default to landing if there is a tie, as landing is generally more time sensitive
-            else:
-                amount_arriving -= 1
-                runway.operating_mode = 'LANDING'
-                runway.temp_optimised = True
-                runway.save()
+        Runway.objects.bulk_update(mixed_runways, ['operating_mode', 'temp_optimised'])
 
     # This function resets all runways back to mixed if they have been optimised
-    def reset_optimised_runways(self) -> None:
-        optimised_runways = Runway.objects.filter(temp_optimised=True)
+    @staticmethod
+    def reset_optimised_runways(self):
+        optimised_runways = Runway.objects.filter(temp_optimised=True, occupied_by__isnull=True)
         for runway in optimised_runways:
             runway.operating_mode = 'MIXED'
             runway.temp_optimised = False
-            runway.save()
+        Runway.objects.bulk_update(optimised_runways, ['operating_mode', 'temp_optimised'])

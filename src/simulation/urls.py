fa58a6101af347864baed70c7724d88b320a39bc
Ethan <ethanwbeard@gmail.com>
2026-03-06 16:15:26 +0000

    Tables working

--- src/simulation/urls.py
+++ src/simulation/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
-from .views import home, results, simulation, stream, liveviewtabular, GetSampleData
+from .views import home, results, simulation, stream, GetSampleData
 
 urlpatterns = [
     path('', home, name='home'),
     path('results/', results, name='results'),
     path('simulation/',simulation, name='simulation'),
-    path('liveviewtabular/',liveviewtabular, name='liveviewtabular'),
-
     path('api/sample-data/', GetSampleData.as_view(), name='sample-data'),
     path('stream/',stream, name='stream')
 
--- src/simulation/views.py
+++ src/simulation/views.py
@@ -42,22 +42,15 @@ def simulation(request):
     #Loads simulation page
     #takeoffQ = Member.objects.all().values()
     #landingQ = Member.objects.all().values()
+    num_runways = 7
 
     template = loader.get_template('pages/simulationtables.html')
     context = {
-       #'takeoffQ': takeoffQ,
-       #'landingQ': landingQ,
-    }
-    return HttpResponse(template.render(context, request))
-
-def liveviewtabular(request):
-
-    num_runways = 7
-    template = loader.get_template('pages/liveviewtabular.html')
-    context = {
         'runway_range': range(1, num_runways + 1)
+
     }
     return HttpResponse(template.render(context, request))
+
 
 
 class GetSampleData(APIView):
